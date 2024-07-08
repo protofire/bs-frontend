48b9541cb31b602e7da030ff2f8d1fb77e0cdebe
Den Smalonski <dzianis.smalonski@protofire.io>
2024-07-08 14:45:11 +0200

    Revert "fix: add support two socket connections"
    This reverts commit 784a2d8d953422223bbcb86cf7bc3daf17646197.

--- lib/socket/context.tsx
+++ lib/socket/context.tsx
@@ -1,11 +1,7 @@
 // https://hexdocs.pm/phoenix/js/
-import { useRouter } from 'next/router';
 import type { SocketConnectOption } from 'phoenix';
 import { Socket } from 'phoenix';
 import React, { useEffect, useState } from 'react';
-
-import config from 'configs/app';
-import useShards from 'lib/hooks/useShards';
 
 export const SocketContext = React.createContext<Socket | null>(null);
 
@@ -15,61 +11,23 @@ interface SocketProviderProps {
   options?: Partial<SocketConnectOption>;
 }
 
-export function SocketProvider({ children, options }: SocketProviderProps) {
+export function SocketProvider({ children, options, url }: SocketProviderProps) {
   const [ socket, setSocket ] = useState<Socket | null>(null);
-  const { shardId, defaultShardId, shards } = useShards();
-  const router = useRouter();
 
   useEffect(() => {
-    const shardIds = Object.keys(shards);
-
-    if (!shardIds.length) {
+    if (!url) {
       return;
     }
-    const _sockets: Array<[string, Socket]> = [];
-    shardIds.forEach(id => {
-      const shardInfo = shards[id];
-      const url = new URL(`${ config.api.socket }${ config.api.basePath }/socket/v2`);
-      if (shardInfo?.apiHost) {
-        url.host = shardInfo.apiHost;
-      }
-      const socketInstance = new Socket(url.toString(), options);
-      socketInstance.connect();
-      _sockets.push([ id, socketInstance ]);
-    });
 
-    const id = shardId ? shardId : defaultShardId;
-    const connection = _sockets.find(elem => {
-      return elem[0] === id;
-    });
-    if (connection) {
-      setSocket(connection[1]);
-    }
-    const handleRouteChange = (urlString: string) => {
-      const query = urlString.split('?')[1];
-      const params = new URLSearchParams(query);
-      const shard = params.get('shard') || '0';
-      if (shard) {
-        const connection = _sockets.find(elem => {
-          return elem[0] === shardId;
-        });
+    const socketInstance = new Socket(url, options);
+    socketInstance.connect();
+    setSocket(socketInstance);
 
-        if (!connection) {
-          return;
-        }
-        setSocket(connection[1]);
-      }
-    };
-    router.events.on('routeChangeStart', handleRouteChange);
     return () => {
-      router.events.off('routeChangeStart', handleRouteChange);
-      _sockets.forEach(element => {
-        element[1].disconnect();
-        setSocket(null);
-      });
+      socketInstance.disconnect();
+      setSocket(null);
     };
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [ options, url ]);
 
   return (
     <SocketContext.Provider value={ socket }>
